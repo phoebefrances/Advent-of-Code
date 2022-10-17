7814f77c13ff9e5a73539eb0ed4d8814aac6fce4
Phoebe Windham <phoebe.frances@outlook.com>
2022-10-17 14:12:37 +0100

    turned puzzleInput.txt file into an array

---
+++ day1/part1/puzzleInput.js
@@ -0,0 +1,14 @@
+const {readFileSync, promises: fsPromises} = require('fs');
+
+function syncReadFile(filename) {
+  const contents = readFileSync(filename, 'utf-8');
+
+  const arr = contents.split(/\r?\n/);
+
+  console.log(arr); 
+
+  return arr;
+}
+
+syncReadFile('./puzzleInput.txt');
+
