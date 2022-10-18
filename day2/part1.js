ebd4ee007695fce869956032711f219c0744d1ad
Phoebe Windham <phoebe.frances@outlook.com>
2022-10-18 18:17:37 +0100

    updated plan, created a function which loops through array of objects with key value pairs command and unit which decreases depth value when command is equal to up and vice versa when command is equal to down and when command is equal to forward horizontal is increased and then prints the multiplication of depth and horizontal final values

--- day2/part1.js
+++ day2/part1.js
@@ -1,3 +1,22 @@
 const input = require('./input.js')
 
-console.log(input);
+let horizontal = 0; 
+let depth = 0; 
+
+function calculatePosition () {
+
+    for(let {command, unit} of input){
+        if (command === 'up') {
+            depth -= unit; 
+        }
+        else if (command === 'down'){
+            depth += unit; 
+        }
+        else if (command === 'forward'){
+            horizontal += unit
+        }
+    }
+}
+
+calculatePosition(input)
+console.log(depth * horizontal)
---
+++ day2/plan.md
@@ -0,0 +1,9 @@
+## Part 1 
+
+- imported array of objects 
+- set horizontal and depth as zero 
+- loop through the array of objects 
+- if statement that says if command is strictly equal to 'up' then decrease the depth by x units
+- else if statment that says if command is strictly equal to 'down' then increase depth by x units 
+- else if statement that says if command is strictly equal to 'forward' then increase horizontal by x units 
+- print depth * horizontal 
